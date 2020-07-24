c890e26c31eadb2c3ffa0055f70214471c500f47
reddevilcero <reddevil_cero@hotmail.com>
2020-07-24 15:17:20 +0100

    create some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+['javaScript', 'Ruby', 'Rails', 'Html', 'Css'].each do |category|
+  
+  Category.creeate(title: category)
+end
