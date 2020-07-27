ef05b5aa0958e6777b13cf2742a447a40d3c76a7
lorrydriveloper <lorrydriveloper@gmail.com>
2020-07-27 18:35:36 +0100

    apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_164049) do
+ActiveRecord::Schema.define(version: 2020_07_27_160734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_07_22_164049) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "color"
+    t.string "imageUrl"
   end
 
 end
