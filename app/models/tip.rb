95864fb249d876b8665ee4e5323264870dc7f1ec
lorrydriveloper <lorrydriveloper@gmail.com>
2020-07-27 19:31:42 +0100

    return the ids of user who has vote

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,4 +1,11 @@
 class Tip < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :votes
+
+  def all_votes
+    # return ids how has already vote this tip
+    # votes.map(&:id) for better performance use below
+    votes.pluck(:id)
+  end
 end
