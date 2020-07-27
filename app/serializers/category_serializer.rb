357ab1962ffddd4b0461642330938d44e071dbfd
lorrydriveloper <lorrydriveloper@gmail.com>
2020-07-27 19:37:57 +0100

    add to serializer total vote in category

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,8 +1,10 @@
 class CategorySerializer < ActiveModel::Serializer
 
-  attributes :title, :color, :imageUrl, :id , :votes
+  attributes :title, :color, :imageUrl, :id , :votes, :tips
 
   def votes
     object.all_votes
   end
+
+ 
 end
