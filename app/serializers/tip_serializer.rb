c077754997efb53c06761fb30755028f9a7521f7
reddevilcero <reddevil_cero@hotmail.com>
2020-07-28 17:25:56 +0100

    add by username attribute

--- app/serializers/tip_serializer.rb
+++ app/serializers/tip_serializer.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class TipSerializer < ActiveModel::Serializer
   attributes :id,
-             :user_id,
              :category_id,
+             :user_id,
+             :by_username,
              :title,
              :description,
              :code_snippet,
@@ -10,5 +13,9 @@ class TipSerializer < ActiveModel::Serializer
 
   def votes
     object.all_votes
+  end
+
+  def by_username
+    object.user.username
   end
 end
