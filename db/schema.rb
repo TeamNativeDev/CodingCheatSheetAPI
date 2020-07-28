e81ce0da38d4f40c16a79a94933f9562e4f09a58
reddevilcero <reddevil_cero@hotmail.com>
2020-07-28 16:47:49 +0100

    rubocop change

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,54 +12,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_170001) do
-
+ActiveRecord::Schema.define(version: 20_200_727_170_001) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "color"
-    t.string "imageUrl"
+  create_table 'categories', force: :cascade do |t|
+    t.string 'title'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'color'
+    t.string 'imageUrl'
   end
 
-  create_table "tips", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "category_id", null: false
-    t.string "title"
-    t.string "description"
-    t.string "code_snippet"
-    t.string "more_info"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_tips_on_category_id"
-    t.index ["user_id"], name: "index_tips_on_user_id"
+  create_table 'tips', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'category_id', null: false
+    t.string 'title'
+    t.string 'description'
+    t.string 'code_snippet'
+    t.string 'more_info'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_tips_on_category_id'
+    t.index ['user_id'], name: 'index_tips_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "username"
-    t.text "bio"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email'
+    t.string 'password_digest'
+    t.string 'username'
+    t.text 'bio'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['username'], name: 'index_users_on_username', unique: true
   end
 
-  create_table "votes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "tip_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tip_id"], name: "index_votes_on_tip_id"
-    t.index ["user_id"], name: "index_votes_on_user_id"
+  create_table 'votes', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'tip_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['tip_id'], name: 'index_votes_on_tip_id'
+    t.index ['user_id'], name: 'index_votes_on_user_id'
   end
 
-  add_foreign_key "tips", "categories"
-  add_foreign_key "tips", "users"
-  add_foreign_key "votes", "tips"
-  add_foreign_key "votes", "users"
+  add_foreign_key 'tips', 'categories'
+  add_foreign_key 'tips', 'users'
+  add_foreign_key 'votes', 'tips'
+  add_foreign_key 'votes', 'users'
 end
