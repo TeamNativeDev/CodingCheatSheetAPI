b6529d2d97fc312f1ed18fe69ed58d9198835a4f
biagioo <mendolia2328@gmail.com>
2020-07-31 11:12:04 -0400

    fixed serializer issue

--- app/controllers/api/v1/tips_controller.rb
+++ app/controllers/api/v1/tips_controller.rb
@@ -2,8 +2,13 @@
 
 class Api::V1::TipsController < ApplicationController
   def create
-    # @tip = Tip.new(tip_params)
-    debugger
+    @tip = Tip.new(tip_params)
+    if @tip.save 
+      render json: {message: 'ok', data: TipSerializer.new(@tip)}
+    else
+      render json: {message: 'notOk'}
+    end
+    # debugheyger
   end
 
   private
