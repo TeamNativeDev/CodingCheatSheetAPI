77f847a7762e9456b90e8d2d6ca0d32f0ef49ea4
reddevilcero <reddevil_cero@hotmail.com>
2020-07-31 14:23:20 +0100

    add route for tips

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories, only: %i[index show create]
+      resources :tips, only: %i[create]
     end
   end
 end
