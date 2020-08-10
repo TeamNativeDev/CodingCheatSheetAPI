42a4f5d9f16cb257da2b551fbe906e6bc13020e7
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-10 19:16:50 +0100

    update with better error messages.

--- app/controllers/api/v1/tips_controller.rb
+++ app/controllers/api/v1/tips_controller.rb
@@ -2,11 +2,11 @@
 
 class Api::V1::TipsController < ApplicationController
   def create
-    @tip = Tip.new(tip_params)
-    if @tip.save 
-      render json: {message: 'ok', data: TipSerializer.new(@tip)}
+    @tip = current_user.tips.build(tip_params)
+    if @tip.save
+      render json: { message: 'Tip Create Successfully', data: TipSerializer.new(@tip), status: :accepted }
     else
-      render json: {message: 'notOk'}
+      render json: { message: 'Unable to Save the Tip', error: true, status: :bad_request }
     end
     # debugheyger
   end
@@ -14,6 +14,6 @@ class Api::V1::TipsController < ApplicationController
   private
 
   def tip_params
-    params.require(:tip).permit(:category_id, :title, :description, :code_snippet, :user_id, :more_info)
+    params.require(:tip).permit(:category_id, :title, :description, :code_snippet, :more_info)
   end
 end
