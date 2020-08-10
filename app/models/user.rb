40bf4e21220d39d3f90d0d354b2f8a513eb77c08
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-10 19:13:52 +0100

    ensure user email an username save as downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tips
   has_many :votes, through: :tips
+  before_validation :lowercase_attr
+  
+
+   def lowercase_attr
+    self.email = email.downcase.strip
+    self.username = username.downcase.strip
+  end
+
+
 end
