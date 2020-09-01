7bd6f264f47c323801800738bd030c6b8a12d4c6
lorrydriveloper <lorrydriveloper@gmail.com>
2020-09-01 17:50:37 +0100

    no edit route needed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories, only: %i[index show create]
-      resources :tips, only: %i[create edit update]
+      resources :tips, only: %i[create update]
       post '/sign_up' => 'sessions#sign_up'
       post '/login' => 'sessions#login'
     end
